58981391c626206638b8f319b7ca1bc7b3038431
kleinnj <kleinnj@rose-hulman.edu>
2013-11-03 20:17:55 -0500

    Added a directory watcher. It is not finished yet, need to know what it will send.

---
+++ src/server/ServletLoader.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.nio.file.*;
+import static java.nio.file.StandardWatchEventKinds.*;
+import static java.nio.file.LinkOption.*;
+import java.nio.file.attribute.*;
+import java.io.*;
+import java.util.*;
+
+public class ServletLoader {
+	
+	public static final String path = "../SimpleWebServer/Servlets/";
+	
+	void DirectoryWatcher(){
+		
+		
+	}
+
+}
